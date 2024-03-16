60e01afa1e8e6a7100fedeef5199e7c114f874ae
klb6542 <keeganthebates2023@gmail.com>
2024-03-16 16:56:17 -0400

    Added colors & formats

---
+++ src/main/java/me/keegan/utils/formatUtil.java
@@ -0,0 +1,32 @@
+package me.keegan.utils;
+
+import org.bukkit.ChatColor;
+
+/*
+ * Copyright (c) 2024. Created by klb.
+ */
+
+public class formatUtil {
+    public static final ChatColor white = ChatColor.WHITE;
+    public static final ChatColor gray = ChatColor.GRAY;
+    public static final ChatColor darkGray = ChatColor.DARK_GRAY;
+    public static final ChatColor black = ChatColor.BLACK;
+    public static final ChatColor red = ChatColor.RED;
+    public static final ChatColor darkRed = ChatColor.DARK_RED;
+    public static final ChatColor gold = ChatColor.GOLD;
+    public static final ChatColor yellow = ChatColor.YELLOW;
+    public static final ChatColor green = ChatColor.GREEN;
+    public static final ChatColor darkGreen = ChatColor.DARK_GREEN;
+    public static final ChatColor aqua = ChatColor.AQUA;
+    public static final ChatColor darkAqua = ChatColor.DARK_AQUA;
+    public static final ChatColor blue = ChatColor.BLUE;
+    public static final ChatColor darkBlue = ChatColor.DARK_BLUE;
+    public static final ChatColor lightPurple = ChatColor.LIGHT_PURPLE;
+    public static final ChatColor darkPurple = ChatColor.DARK_PURPLE;
+    public static final ChatColor bold = ChatColor.BOLD;
+    public static final ChatColor italic = ChatColor.ITALIC;
+    public static final ChatColor underline = ChatColor.UNDERLINE;
+    public static final ChatColor strikethrough = ChatColor.STRIKETHROUGH;
+    public static final ChatColor magic = ChatColor.MAGIC;
+    public static final ChatColor reset = ChatColor.RESET;
+}
