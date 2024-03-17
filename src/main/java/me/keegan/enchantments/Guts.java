718910a272e65436984b3b24c7ad348086e72f94
klb6542 <keeganthebates2023@gmail.com>
2024-03-17 12:25:17 -0400

    Improved methods

--- src/main/java/me/keegan/enchantments/Guts.java
+++ src/main/java/me/keegan/enchantments/Guts.java
@@ -2,6 +2,12 @@ package me.keegan.enchantments;
 
 import me.keegan.utils.enchantUtil;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
 import java.text.MessageFormat;
 
 import static me.keegan.utils.formatUtil.*;
@@ -10,7 +16,9 @@ import static me.keegan.utils.formatUtil.*;
  * Copyright (c) 2024. Created by klb.
  */
 
-public class Guts extends enchantUtil {
+public class Guts extends enchantUtil implements Listener {
+    private final Double[] healingPerLevel = new Double[]{0.25, 0.5, 1.0};
+
     @Override
     public Material[] getEnchantMaterial() {
         return new Material[]{Material.GOLDEN_SWORD};
@@ -29,15 +37,15 @@ public class Guts extends enchantUtil {
     @Override
     public String[] getEnchantDescription() {
         return new String[]{
-                MessageFormat.format("{0}Heal {1}0.25❤{0} on kill", gray, red),
-                MessageFormat.format("{0}Heal {1}0.5❤{0} on kill", gray, red),
-                MessageFormat.format("{0}Heal {1}1❤{0} on kill", gray, red)
+                MessageFormat.format("{0}Heal {1}{2}❤{0} on kill", gray, red, healingPerLevel[0]),
+                MessageFormat.format("{0}Heal {1}{2}❤{0} on kill", gray, red, healingPerLevel[1]),
+                MessageFormat.format("{0}Heal {1}{2}❤{0} on kill", gray, red, healingPerLevel[2])
         };
     }
 
     @Override
-    public Number getMaxLevel() {
-        return 3;
+    public Integer getMaxLevel() {
+        return this.healingPerLevel.length;
     }
 
     @Override
@@ -46,7 +54,17 @@ public class Guts extends enchantUtil {
     }
 
     @Override
-    public void procEnchant() {
+    public void procEnchant(LivingEntity damaged, LivingEntity damager, Object... args) {
+
+    }
+
+    @EventHandler
+    public void entityDamaged(EntityDamageByEntityEvent e) {
+        Entity damaged = e.getEntity();
+        Entity damager = e.getDamager();
+
+        if (!(damaged instanceof LivingEntity) || !(damager instanceof  LivingEntity)) { return; }
+
 
     }
 }
