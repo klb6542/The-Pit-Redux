7ef30ed4ea285e64295f2afed68df4d6703dd1ce
klb6542 <keeganthebates2023@gmail.com>
2024-03-16 21:10:55 -0400

    Changed enum enchantTypes to an abstract method that returns an array of materials

--- src/main/java/me/keegan/utils/enchantUtil.java
+++ src/main/java/me/keegan/utils/enchantUtil.java
@@ -4,14 +4,10 @@ package me.keegan.utils;
  * Copyright (c) 2024. Created by klb.
  */
 
-public abstract class enchantUtil {
-    public enum enchantTypes {
-        SWORD,
-        BOW,
-        PANTS
-    }
+import org.bukkit.Material;
 
-    public abstract enchantTypes[] getEnchantType();
+public abstract class enchantUtil {
+    public abstract Material[] getEnchantMaterial();
     public abstract String getName();
     public abstract String getEnchantName();
     public abstract String[] getEnchantDescription();
