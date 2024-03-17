0a4f3832205b1e1beba92f27825ad498614ccb04
klb6542 <keeganthebates2023@gmail.com>
2024-03-16 21:11:03 -0400

    Initial Commit

---
+++ src/main/java/me/keegan/handlers/entityDamageHandler.java
@@ -0,0 +1,12 @@
+package me.keegan.handlers;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class entityDamageHandler implements Listener {
+    @EventHandler
+    public void entityDamaged(EntityDamageEvent e) {
+
+    }
+}
