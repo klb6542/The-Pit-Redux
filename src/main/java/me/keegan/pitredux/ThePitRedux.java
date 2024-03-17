e6a97b3808903fea253b8783d99db5f6c0730927
klb6542 <keeganthebates2023@gmail.com>
2024-03-16 21:12:31 -0400

    Added registerEvents method

--- src/main/java/me/keegan/pitredux/ThePitRedux.java
+++ src/main/java/me/keegan/pitredux/ThePitRedux.java
@@ -1,5 +1,7 @@
 package me.keegan.pitredux;
 
+import me.keegan.handlers.entityDamageHandler;
+import me.keegan.handlers.playerDamageHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /*
@@ -9,10 +11,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class ThePitRedux extends JavaPlugin {
     private static ThePitRedux plugin;
 
+    private void registerEvents() {
+        getServer().getPluginManager().registerEvents(new playerDamageHandler(), this);
+        getServer().getPluginManager().registerEvents(new entityDamageHandler(), this);
+    }
+
     @Override
     public void onEnable() {
         plugin = this;
         getLogger().info("Hello, world");
+
+        registerEvents();
     }
 
     @Override
