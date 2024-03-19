fd2c61c3d749a9733e35c3cc7daf33a8f84a3013
klb6542 <keeganthebates2023@gmail.com>
2024-03-19 18:04:50 -0400

    Finished attemptEnchantExecution method with comments

--- src/main/java/me/keegan/utils/enchantUtil.java
+++ src/main/java/me/keegan/utils/enchantUtil.java
@@ -4,10 +4,11 @@ package me.keegan.utils;
  * Copyright (c) 2024. Created by klb.
  */
 
+import me.keegan.pitredux.ThePitRedux;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
 
 public abstract class enchantUtil implements Listener {
     public abstract Material[] getEnchantMaterial();
@@ -15,9 +16,32 @@ public abstract class enchantUtil implements Listener {
     public abstract String[] getEnchantDescription();
     public abstract Integer getMaxLevel();
     public abstract boolean isRareEnchant();
-    public abstract void executeEnchant(LivingEntity damaged, LivingEntity damager, EntityDamageByEntityEvent e, Object... args);
+    public abstract void executeEnchant(Object... args);
 
-    public void attemptEnchantExecution(LivingEntity damaged, LivingEntity damager, EntityDamageByEntityEvent e, Object... args) {
-        this.executeEnchant(damaged, damager, e, args);
+    // below is included incase there is an enchantment to reduce/cancel healing or any other methods
+    public void heal(LivingEntity entity, Double amount) {
+        entity.setHealth(entity.getHealth() + amount);
+    }
+
+    /*
+     * Object... args contains:
+
+     * args[0] = event
+     * args[1] = itemstack to check for enchant
+     * args[2] = enchant that is being attempted to execute
+     -------------------------------------------------------------------------------------------------------------------
+     * Object... args returns:
+
+     * args[0] = event
+     * args[1] = itemstack
+     * args[2] = enchant level - 1
+     */
+
+    public void attemptEnchantExecution(Object[] args) {
+        if (args[1] == null) { return; }
+        if (!mysticUtil.getInstance().hasEnchant((ItemStack) args[1], (enchantUtil) args[2])) { return; }
+
+        args[2] = mysticUtil.getInstance().getEnchantLevel((ItemStack) args[1], (enchantUtil) args[2]) - 1;
+        this.executeEnchant(args);
     }
 }
