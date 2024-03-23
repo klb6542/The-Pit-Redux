c070609315dd0aa5d604f402f1b59bbee11682ee
klb6542 <keeganthebates2023@gmail.com>
2024-03-23 14:27:19 -0400

    Added wordUtil

---
+++ src/main/java/me/keegan/utils/wordUtil.java
@@ -0,0 +1,21 @@
+package me.keegan.utils;
+
+/*
+ * Copyright (c) 2024. Created by klb.
+ */
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class wordUtil {
+    private static final List<String> words = new ArrayList<>(Arrays.asList(
+            "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"
+    ));
+
+    public static @Nullable String integerToWord(int number) {
+        return (number >= 10 || number < 1) ? "zero" : words.get(number - 1);
+    }
+}
