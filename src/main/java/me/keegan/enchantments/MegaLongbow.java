98c8775352743403c2e4b07e145e077a4b03b398
klb6542 <keeganthebates2023@gmail.com>
2024-03-23 14:29:54 -0400

    Added copyright message, and also added a cooldown for Mega Longbow

--- src/main/java/me/keegan/enchantments/MegaLongbow.java
+++ src/main/java/me/keegan/enchantments/MegaLongbow.java
@@ -1,5 +1,6 @@
 package me.keegan.enchantments;
 
+import me.keegan.enums.cooldownEnums;
 import me.keegan.utils.enchantUtil;
 import org.bukkit.Material;
 import org.bukkit.entity.Arrow;
@@ -12,6 +13,10 @@ import java.text.MessageFormat;
 import static me.keegan.utils.formatUtil.gray;
 import static me.keegan.utils.formatUtil.green;
 import static me.keegan.utils.romanUtil.integerToRoman;
+
+/*
+ * Copyright (c) 2024. Created by klb.
+ */
 
 public class MegaLongbow extends enchantUtil {
     private final Integer[] jumpBoostAmplifierPerLevel = new Integer[]{1, 2, 3};
@@ -32,15 +37,15 @@ public class MegaLongbow extends enchantUtil {
         return new String[]{
                 MessageFormat.format("{0}One shot per second, this bow is/n" +
                         "{0}automatically fully drawn and/n" +
-                        "{0}grants {1}Jump Boost {2} ({3}s)", gray, green, integerToRoman(jumpBoostAmplifierPerLevel[0], false), jumpBoostDuration),
+                        "{0}grants {1}Jump Boost {2} ({3}s)", gray, green, integerToRoman(jumpBoostAmplifierPerLevel[0] + 1, false), jumpBoostDuration),
 
                 MessageFormat.format("{0}One shot per second, this bow is/n" +
                         "{0}automatically fully drawn and/n" +
-                        "{0}grants {1}Jump Boost {2} ({3}s)", gray, green, integerToRoman(jumpBoostAmplifierPerLevel[1], false), jumpBoostDuration),
+                        "{0}grants {1}Jump Boost {2} ({3}s)", gray, green, integerToRoman(jumpBoostAmplifierPerLevel[1] + 1, false), jumpBoostDuration),
 
                 MessageFormat.format("{0}One shot per second, this bow is/n" +
                         "{0}automatically fully drawn and/n" +
-                        "{0}grants {1}Jump Boost {2} ({3}s)", gray, green, integerToRoman(jumpBoostAmplifierPerLevel[2], false), jumpBoostDuration),
+                        "{0}grants {1}Jump Boost {2} ({3}s)", gray, green, integerToRoman(jumpBoostAmplifierPerLevel[2] + 1, false), jumpBoostDuration),
         };
     }
 
@@ -61,6 +66,9 @@ public class MegaLongbow extends enchantUtil {
         EntityShootBowEvent e = (EntityShootBowEvent) args[0];
         LivingEntity shooter = e.getEntity();
         Arrow arrow = (Arrow) e.getProjectile();
+
+        if (this.isOnCooldown(shooter.getUniqueId())) { return; }
+        this.addCooldown(shooter.getUniqueId(), (long) 1.01, cooldownEnums.NORMAL);
 
         arrow.setCritical(true);
         arrow.setVelocity(shooter.getLocation().getDirection().multiply(2.90));
