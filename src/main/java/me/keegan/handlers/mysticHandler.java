2bb6aca67f7869b198cfea09dfdd9cc083962f17
klb6542 <keeganthebates2023@gmail.com>
2024-03-24 18:09:48 -0400

    Added listener that disables mystics from being enchanted

--- src/main/java/me/keegan/handlers/mysticHandler.java
+++ src/main/java/me/keegan/handlers/mysticHandler.java
@@ -1,20 +1,11 @@
 package me.keegan.handlers;
 
-import me.keegan.pitredux.ThePitRedux;
-import org.bukkit.NamespacedKey;
+import me.keegan.utils.mysticUtil;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.persistence.PersistentDataType;
 
 public class mysticHandler implements Listener {
     public void enchantItem(EnchantItemEvent e) {
-        ItemStack itemStack = e.getItem();
-        if (itemStack.getItemMeta() == null) { return; }
-
-        if (itemStack.getItemMeta().getPersistentDataContainer()
-                .has(new NamespacedKey(ThePitRedux.getPlugin(), "mystic"), PersistentDataType.STRING)) {
-            e.setCancelled(true);
-        }
+        if (mysticUtil.getInstance().isMystic(e.getItem())) { e.setCancelled(true); }
     }
 }
