19c9356a97501ab64bbf5750cc06ffe97f8023ed
klb6542 <keeganthebates2023@gmail.com>
2024-03-27 12:20:38 -0400

    Added more methods to stop enchanting mystics

--- src/main/java/me/keegan/handlers/mysticHandler.java
+++ src/main/java/me/keegan/handlers/mysticHandler.java
@@ -1,11 +1,21 @@
 package me.keegan.handlers;
 
 import me.keegan.utils.mysticUtil;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class mysticHandler implements Listener {
-    public void enchantItem(EnchantItemEvent e) {
+    @EventHandler
+    public void prepareAnvil(PrepareAnvilEvent e) {
+        if (mysticUtil.getInstance().isMystic(e.getResult())) { e.setResult(new ItemStack(Material.AIR)); }
+    }
+
+    @EventHandler
+    public void prepareItemEnchant(PrepareItemEnchantEvent e) {
         if (mysticUtil.getInstance().isMystic(e.getItem())) { e.setCancelled(true); }
     }
 }
