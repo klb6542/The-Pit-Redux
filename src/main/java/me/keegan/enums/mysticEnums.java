06be4dfaaa8cd0508f51f65261f7ff073c2a4d3a
klb6542 <keeganthebates2023@gmail.com>
2024-03-31 22:39:17 -0400

    Creation of mysticEnums

---
+++ src/main/java/me/keegan/enums/mysticEnums.java
@@ -0,0 +1,14 @@
+package me.keegan.enums;
+
+/*
+ * Copyright (c) 2024. Created by klb.
+ */
+
+public enum mysticEnums {
+    /*
+     * NORMAL - normal mystics like dyed pants, mystic swords, and mystics bows (other materials soon)
+     * DARK - dark pants like in pit
+     */
+    NORMAL,
+    DARK,
+}
