619782f7a0eb059dfe1995f567b48d2c3c003d3c
klb6542 <keeganthebates2023@gmail.com>
2024-04-01 22:05:27 -0400

    Creation of Stereo enchant

---
+++ src/main/java/me/keegan/enchantments/Stereo.java
@@ -0,0 +1,126 @@
+package me.keegan.enchantments;
+
+import com.xxmicloxx.NoteBlockAPI.model.Playlist;
+import com.xxmicloxx.NoteBlockAPI.model.Song;
+import com.xxmicloxx.NoteBlockAPI.utils.NBSDecoder;
+import me.keegan.enums.mysticEnums;
+import me.keegan.pitredux.ThePitRedux;
+import me.keegan.utils.enchantUtil;
+import me.keegan.utils.setupUtils;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.File;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import static me.keegan.utils.formatUtil.gray;
+
+public class Stereo extends enchantUtil implements setupUtils {
+    private static final HashMap<Player, BukkitRunnable> runnables = new HashMap<>();
+    private static final List<Player> stereoPantsEquipped = new ArrayList<>();
+
+    private final long timerPeriod = 5L;
+
+    private final File songsDirectory = new File("src/main/resources/songs");
+
+   // private final Playlist stereoPlaylist = new Playlist(
+     //       (Song) Arrays.stream(songsDirectory.listFiles()).iterator()
+   // );
+
+    @Override
+    public Material[] getEnchantMaterial() {
+        return new Material[]{Material.LEATHER_LEGGINGS};
+    }
+
+    @Override
+    public mysticEnums getEnchantType() {
+        return mysticEnums.NORMAL;
+    }
+
+    @Override
+    public String getName() {
+        return "Stereo";
+    }
+
+    @Override
+    public String[] getEnchantDescription() {
+        return new String[]{
+                MessageFormat.format("{0}You play a tune while cruising/n" +
+                        "{0}around", gray)
+        };
+    }
+
+    @Override
+    public Integer getMaxLevel() {
+        return this.getEnchantDescription().length;
+    }
+
+    @Override
+    public boolean isRareEnchant() {
+        return true;
+    }
+
+    @Override
+    public void executeEnchant(Object[] args) {
+        PlayerJoinEvent e = (PlayerJoinEvent) args[0];
+        Player player = e.getPlayer();
+
+        stereoPantsEquipped.add(player);
+
+
+    }
+
+    @EventHandler
+    public void playerJoined(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+
+        runnables.put(player, new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                if (player.getEquipment() == null
+                        || player.getEquipment().getLeggings() == null
+                        || stereoPantsEquipped.contains(player)) { return; }
+
+                Object[] args = new Object[]{
+                        e,
+                        player.getEquipment().getLeggings(),
+                        this
+                };
+
+                attemptEnchantExecution(args);
+            }
+
+        }).runTaskTimer(ThePitRedux.getPlugin(), 0, timerPeriod);
+    }
+
+    @EventHandler
+    public void playerLeft(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+
+        if (stereoPantsEquipped.contains(player)) {
+            stereoPantsEquipped.remove(player);
+        }
+
+        runnables.get(player).cancel();
+        runnables.remove(player);
+    }
+
+    @Override
+    public void pluginEnabled() {
+
+    }
+
+    @Override
+    public void pluginDisabled() {
+
+    }
+}
