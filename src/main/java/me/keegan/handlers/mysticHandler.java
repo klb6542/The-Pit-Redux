41bb39a390a32e262fce7da8015f6d8a8bce1358
klb6542 <keeganthebates2023@gmail.com>
2024-04-06 20:41:34 -0400

    Removed the ability to dye mystics pants

--- src/main/java/me/keegan/handlers/mysticHandler.java
+++ src/main/java/me/keegan/handlers/mysticHandler.java
@@ -5,7 +5,9 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
 
 /*
@@ -21,5 +23,10 @@ public class mysticHandler implements Listener {
     @EventHandler
     public void prepareItemEnchant(PrepareItemEnchantEvent e) {
         if (mysticUtil.getInstance().isMystic(e.getItem())) { e.setCancelled(true); }
+    }
+
+    @EventHandler
+    public void dyeArmor(CraftItemEvent e) {
+        if (mysticUtil.getInstance().isMystic(e.getCurrentItem())) { e.setCancelled(true); }
     }
 }
