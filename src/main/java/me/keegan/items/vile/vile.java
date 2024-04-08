1eab2f8804ccb3e6a1e99a59c5a97a5d5d5c2539
klb6542 <keeganthebates2023@gmail.com>
2024-04-08 08:07:20 -0400

    Creation of vile

---
+++ src/main/java/me/keegan/items/vile/vile.java
@@ -0,0 +1,65 @@
+package me.keegan.items.vile;
+
+import me.keegan.builders.mystic;
+import me.keegan.pitredux.ThePitRedux;
+import me.keegan.utils.itemUtil;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.FurnaceBurnEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static me.keegan.utils.formatUtil.*;
+
+public class vile extends itemUtil {
+    @Override
+    public String getNamespaceName() {
+        return "vile";
+    }
+
+    @Override
+    public ItemStack createItem() {
+        ItemStack itemStack = new ItemStack(Material.COAL);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+
+        itemMeta.setDisplayName(darkPurple + "Chunk of Vile");
+        List<String> lore = new ArrayList<>();
+
+        lore.add(mystic.defaultLore.get(0));
+        lore.add("");
+        lore.add(red + "Heretic artifact");
+
+        itemMeta.setLore(lore);
+        itemStack.setItemMeta(itemMeta);
+
+        return itemStack;
+    }
+
+    @Override
+    public void createRecipe() {
+        NamespacedKey key = new NamespacedKey(
+                ThePitRedux.getPlugin(),
+                this.getNamespaceName());
+
+        ItemStack vile = this.createItem();
+        vile.setAmount(9);
+
+        ShapelessRecipe recipe = new ShapelessRecipe(key, vile);
+        RecipeChoice vile_block = new RecipeChoice.ExactChoice(new vile_block().createItem());
+
+        recipe.addIngredient(vile_block);
+
+        ThePitRedux.getPlugin().getServer().addRecipe(recipe);
+    }
+
+    @EventHandler
+    public void furnaceBurn(FurnaceBurnEvent e) {
+        if (e.getFuel().isSimilar(this.createItem())) { e.setCancelled(true); }
+    }
+}
