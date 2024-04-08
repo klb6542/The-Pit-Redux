092afdb757129a2fdccc10c8bd73f125f2a1068c
klb6542 <keeganthebates2023@gmail.com>
2024-04-08 08:03:30 -0400

    Moved dyes list in another class

--- src/main/java/me/keegan/mysticwell/animation.java
+++ src/main/java/me/keegan/mysticwell/animation.java
@@ -10,6 +10,8 @@ import org.bukkit.scheduler.BukkitTask;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
+import static me.keegan.utils.itemUtil.dyes;
+
 /*
  * Copyright (c) 2024. Created by klb.
  */
@@ -19,25 +21,6 @@ public class animation {
     private final List<ItemStack> glassPanes;
 
     private final List<BukkitTask> runnableTasks = new ArrayList<>();
-
-    private final List<Material> dyes = new ArrayList<>(Arrays.asList(
-            Material.YELLOW_DYE,
-            Material.BLACK_DYE,
-            Material.BLUE_DYE,
-            Material.GREEN_DYE,
-            Material.CYAN_DYE,
-            Material.GRAY_DYE,
-            Material.BROWN_DYE,
-            Material.LIGHT_BLUE_DYE,
-            Material.LIGHT_GRAY_DYE,
-            Material.LIME_DYE,
-            Material.MAGENTA_DYE,
-            Material.ORANGE_DYE,
-            Material.PINK_DYE,
-            Material.PURPLE_DYE,
-            Material.RED_DYE,
-            Material.WHITE_DYE
-        ));
 
     // itemstack is mystic currently being enchanted
     protected ItemStack itemStack = new ItemStack(Material.AIR);
