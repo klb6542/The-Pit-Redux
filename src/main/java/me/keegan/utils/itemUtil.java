1b125b2b64ea29269f806747060159629ec746b2
klb6542 <keeganthebates2023@gmail.com>
2024-04-08 08:04:44 -0400

    Moved dyes list in itemUtil

--- src/main/java/me/keegan/utils/itemUtil.java
+++ src/main/java/me/keegan/utils/itemUtil.java
@@ -1,10 +1,12 @@
 package me.keegan.utils;
 
 import me.keegan.pitredux.ThePitRedux;
+import org.bukkit.Material;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -12,7 +14,24 @@ import java.util.List;
  */
 
 public abstract class itemUtil implements Listener {
-    public static final List<itemUtil> items = new ArrayList<>();
+    public static final List<Material> dyes = new ArrayList<>(Arrays.asList(
+            Material.YELLOW_DYE,
+            Material.BLACK_DYE,
+            Material.BLUE_DYE,
+            Material.GREEN_DYE,
+            Material.CYAN_DYE,
+            Material.GRAY_DYE,
+            Material.BROWN_DYE,
+            Material.LIGHT_BLUE_DYE,
+            Material.LIGHT_GRAY_DYE,
+            Material.LIME_DYE,
+            Material.MAGENTA_DYE,
+            Material.ORANGE_DYE,
+            Material.PINK_DYE,
+            Material.PURPLE_DYE,
+            Material.RED_DYE,
+            Material.WHITE_DYE
+    ));
 
     public abstract String getNamespaceName();
     public abstract ItemStack createItem();
@@ -22,6 +41,5 @@ public abstract class itemUtil implements Listener {
        ThePitRedux.getPlugin().getServer().getPluginManager().registerEvents(item, ThePitRedux.getPlugin());
 
        item.createRecipe();
-       items.add(item);
     }
 }
