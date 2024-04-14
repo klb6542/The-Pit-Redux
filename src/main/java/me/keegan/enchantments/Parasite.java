81683cfd8cda5703327dd8e41b6ab0a3e06d3142
klb6542 <keeganthebates2023@gmail.com>
2024-04-14 17:34:53 -0400

    Added Parasite enchant

---
+++ src/main/java/me/keegan/enchantments/Parasite.java
@@ -0,0 +1,90 @@
+package me.keegan.enchantments;
+
+import me.keegan.enums.mysticEnums;
+import me.keegan.utils.enchantUtil;
+import me.keegan.utils.entityUtil;
+import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+import java.text.MessageFormat;
+
+import static me.keegan.utils.formatUtil.*;
+
+public class Parasite extends enchantUtil {
+    private final Double[] healingAmountPerLevel = new Double[]{0.25, 0.5, 1.0};
+
+    @Override
+    public Material[] getEnchantMaterial() {
+        return new Material[]{Material.BOW};
+    }
+
+    @Override
+    public mysticEnums getEnchantType() {
+        return mysticEnums.NORMAL;
+    }
+
+    @Override
+    public String getName() {
+        return "Parasite";
+    }
+
+    @Override
+    public String[] getEnchantDescription() {
+        return new String[]{
+                MessageFormat.format("{0}Heal {1}{2}❤{0} on arrow hit",
+                        gray, red, healingAmountPerLevel[0]),
+
+                MessageFormat.format("{0}Heal {1}{2}❤{0} on arrow hit",
+                        gray, red, healingAmountPerLevel[1]),
+
+                MessageFormat.format("{0}Heal {1}{2}❤{0} on arrow hit",
+                        gray, red, healingAmountPerLevel[2])
+        };
+    }
+
+    @Override
+    public Integer getMaxLevel() {
+        return this.getEnchantDescription().length;
+    }
+
+    @Override
+    public boolean isRareEnchant() {
+        return false;
+    }
+
+    @Override
+    public boolean isMysticWellEnchant() {
+        return true;
+    }
+
+    @Override
+    public void executeEnchant(Object[] args) {
+        int enchantLevel = (int) args[2];
+
+        EntityDamageByEntityEvent e = (EntityDamageByEntityEvent) args[0];
+        LivingEntity shooter = (LivingEntity) ((Arrow) e.getDamager()).getShooter();
+
+        this.heal(shooter, healingAmountPerLevel[enchantLevel]);
+    }
+
+    @EventHandler
+    public void arrowDamaged(EntityDamageByEntityEvent e) {
+        if (!entityUtil.damagerIsArrow(e)) { return; }
+
+        Arrow arrow = (Arrow) e.getDamager();
+        LivingEntity shooter = (LivingEntity) arrow.getShooter();
+
+        Object[] args = new Object[]{
+                e,
+                (shooter.getEquipment() != null && shooter.getEquipment().getItemInMainHand().getType() == Material.BOW)
+                        ? shooter.getEquipment().getItemInMainHand()
+                        : shooter.getEquipment().getItemInOffHand(),
+                this
+        };
+
+        this.attemptEnchantExecution(args);
+    }
+}
