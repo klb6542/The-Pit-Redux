4906b57cefa69f5a982bebb156e5560fa9ed3f5a
klb6542 <keeganthebates2023@gmail.com>
2024-04-14 21:08:01 -0400

    Creation of Explosive enchant

---
+++ src/main/java/me/keegan/enchantments/Explosive.java
@@ -0,0 +1,68 @@
+package me.keegan.enchantments;
+
+import me.keegan.enums.mysticEnums;
+import me.keegan.utils.enchantUtil;
+import me.keegan.utils.entityUtil;
+import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.ProjectileHitEvent;
+
+public class Explosive extends enchantUtil {
+    @Override
+    public Material[] getEnchantMaterial() {
+        return new Material[]{Material.BOW};
+    }
+
+    @Override
+    public mysticEnums getEnchantType() {
+        return mysticEnums.NORMAL;
+    }
+
+    @Override
+    public String getName() {
+        return "Explosive";
+    }
+
+    @Override
+    public String[] getEnchantDescription() {
+        return new String[]{};
+    }
+
+    @Override
+    public Integer getMaxLevel() {
+        return this.getEnchantDescription().length;
+    }
+
+    @Override
+    public boolean isRareEnchant() {
+        return true;
+    }
+
+    @Override
+    public boolean isMysticWellEnchant() {
+        return true;
+    }
+
+    @Override
+    public void executeEnchant(Object[] args) {
+
+    }
+
+    @EventHandler
+    public void projectileHit(ProjectileHitEvent e) {
+        if (!entityUtil.projectileIsArrow(e)) { return; }
+        LivingEntity shooter = (LivingEntity) e.getEntity().getShooter();
+
+        Object[] args = new Object[]{
+                e,
+                (shooter.getEquipment() != null && shooter.getEquipment().getItemInMainHand().getType() == Material.BOW)
+                        ? shooter.getEquipment().getItemInMainHand()
+                        : shooter.getEquipment().getItemInOffHand(),
+                this
+        };
+
+        this.attemptEnchantExecution(args);
+    }
+}
