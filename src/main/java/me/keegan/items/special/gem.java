84b5bfb4827452c26ba744e02d0679d6f54e6be0
klb6542 <keeganthebates2023@gmail.com>
2024-04-14 21:08:10 -0400

    Creation of Gem

---
+++ src/main/java/me/keegan/items/special/gem.java
@@ -0,0 +1,21 @@
+package me.keegan.items.special;
+
+import me.keegan.utils.itemUtil;
+import org.bukkit.inventory.ItemStack;
+
+public class gem extends itemUtil {
+    @Override
+    public String getNamespaceName() {
+        return "gem";
+    }
+
+    @Override
+    public ItemStack createItem() {
+        return null;
+    }
+
+    @Override
+    public void createRecipe() {
+
+    }
+}
