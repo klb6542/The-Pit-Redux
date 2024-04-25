d5a13d1a500519d195b9d5661400b6a0b31d3bf6
klb6542 <keeganthebates2023@gmail.com>
2024-04-24 22:20:55 -0400

    Creation of Philosopher's Cactus

---
+++ src/main/java/me/keegan/items/special/philosophers_cactus.java
@@ -0,0 +1,58 @@
+package me.keegan.items.special;
+
+import me.keegan.builders.mystic;
+import me.keegan.items.vile.vile_block;
+import me.keegan.pitredux.ThePitRedux;
+import me.keegan.utils.itemUtil;
+import me.keegan.utils.propertiesUtil;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static me.keegan.utils.formatUtil.*;
+import static me.keegan.utils.formatUtil.yellow;
+
+public class philosophers_cactus extends itemUtil {
+    @Override
+    public String getNamespaceName() {
+        return "philosophers_cactus";
+    }
+
+    @Override
+    public ItemStack createItem() {
+        ItemStack itemStack = new ItemStack(Material.CACTUS);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+
+        itemMeta.setDisplayName(green + "Philosopher's Cactus");
+        List<String> lore = new ArrayList<>();
+
+        lore.add(yellow + "Special Item");
+        lore.add(gray + "Right-click while holding");
+        lore.add(gray + "this item to summon fresh");
+        lore.add(red + "P" + gold + "a" + yellow + "n" + green + "t" + blue + "s" + gray + " of your");
+        lore.add(gray + "choice.");
+        lore.add("");
+        lore.add(gray + "(Special pants excluded)");
+
+        propertiesUtil.setProperty(propertiesUtil.notBurnable, itemMeta);
+        propertiesUtil.setProperty(propertiesUtil.notPlaceable, itemMeta);
+
+        itemMeta.setLore(lore);
+        itemStack.setItemMeta(itemMeta);
+
+        return itemStack;
+    }
+
+    @Override
+    public void createRecipe() {
+
+    }
+}
