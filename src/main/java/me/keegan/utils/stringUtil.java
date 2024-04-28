0c429fa04769efc3f772598f4a24b4036fdbfcad
qsmh6542 <keeganlbates2022@gmail.com>
2024-04-27 22:44:07 -0400

    Added more methods

--- src/main/java/me/keegan/utils/stringUtil.java
+++ src/main/java/me/keegan/utils/stringUtil.java
@@ -1,6 +1,16 @@
 package me.keegan.utils;
 
+import java.util.HashMap;
+
 public class stringUtil {
+    private static final HashMap<String, String> pluralWords = new HashMap<String, String>(){{
+        put("zombie", "zombies");
+        put("creeper", "creepers");
+        put("spider", "spiders");
+        put("enderman", "endermen");
+        put("skeleton", "skeletons");
+    }};
+
     public static Integer findKeywordIndex(String[] splitString, String keyWord) {
         for (int i = 0; i < splitString.length; i++) {
             if (!splitString[i].contains(keyWord)) { continue; }
@@ -9,5 +19,13 @@ public class stringUtil {
         }
 
         return -1;
+    }
+
+    public static String upperCaseFirstLetter(String string) {
+        return Character.toUpperCase(string.charAt(0)) + string.toLowerCase().substring(1);
+    }
+
+    public static String getPluralWord(String string) {
+        return pluralWords.getOrDefault(string.toLowerCase(), string);
     }
 }
