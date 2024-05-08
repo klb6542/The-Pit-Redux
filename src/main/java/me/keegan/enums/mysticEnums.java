6d020bd24c8bad0b1bbddf9e74a8de3e592ea3dc
klb6542 <keeganthebates2023@gmail.com>
2024-05-07 21:50:30 -0400

    Added HELMET enum

--- src/main/java/me/keegan/enums/mysticEnums.java
+++ src/main/java/me/keegan/enums/mysticEnums.java
@@ -7,8 +7,10 @@ package me.keegan.enums;
 public enum mysticEnums {
     /*
      * NORMAL - normal mystics like dyed pants, mystic swords, and mystics bows (other materials soon)
+     * HELMET - helmet mystics like the royalty helmet (golden helmet in pit)
      * DARK - dark pants like in pit
      */
     NORMAL,
+    HELMET,
     DARK,
 }
