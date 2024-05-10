949b776a8f3a2c97edd1b26b423b08809cc18bec
klb6542 <keeganthebates2023@gmail.com>
2024-05-09 21:34:10 -0400

    Removed Dark mysticEnum

--- src/main/java/me/keegan/enums/mysticEnums.java
+++ src/main/java/me/keegan/enums/mysticEnums.java
@@ -8,9 +8,7 @@ public enum mysticEnums {
     /*
      * NORMAL - normal mystics like dyed pants, mystic swords, and mystics bows (other materials soon)
      * HELMET - helmet mystics like the royalty helmet (golden helmet in pit)
-     * DARK - dark pants like in pit
      */
     NORMAL,
-    HELMET,
-    DARK,
+    HELMET
 }
